8a6880d180119fefa02a8dc5486fdc3fbc188102
Dave Z <dazig@pop-os.localdomain>
2021-05-24 18:27:58 -0400

    rendering Keeps on App.js and added a card UI wrapper that can be reused

---
+++ authenticate-me/frontend/src/components/Keeps/index.js
@@ -0,0 +1,15 @@
+// import React from "react";
+import Card from "../Ui/Card";
+import "./Keeps.css";
+
+const Keeps = () => {
+  return (
+    <Card>
+      <div>
+        <h1>This is a Keep!</h1>
+      </div>
+    </Card>
+  );
+};
+
+export default Keeps;

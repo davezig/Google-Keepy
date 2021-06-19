8ada2a24575ab82f1e9d874c839f9e52e92db081
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-06-18 21:07:21 -0400

    the new keep input is now working with close button

--- authenticate-me/frontend/src/components/NewKeep/index.js
+++ authenticate-me/frontend/src/components/NewKeep/index.js
@@ -1,15 +1,43 @@
 import { useState } from 'react';
+import './NewKeep.css';
 
 function NewKeep() {
   const [body, setBody] = useState('');
+  const [title, setTitle] = useState('');
+  const [hasFocus, setHasFocus] = useState(false);
+
+  function getFocus(event) {
+    setHasFocus(true);
+  }
+
+  function loseFocus(event) {
+    event.stopPropagation();
+    setHasFocus(false);
+  }
+
   return (
-    <>
+    <div className="newKeep" onClick={getFocus}>
+      {hasFocus && (
+        <input
+          type="text"
+          value={title}
+          placeholder="Title"
+          onInput={(event) => setTitle(event.target.value)}
+        ></input>
+      )}
+
       <input
         type="text"
         value={body}
+        placeholder="Take a note..."
         onInput={(event) => setBody(event.target.value)}
       ></input>
-    </>
+      {hasFocus && (
+        <p className="newKeep__closeButton" onClick={loseFocus}>
+          Close
+        </p>
+      )}
+    </div>
   );
 }
 

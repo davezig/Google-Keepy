a72d647015fa3a17a5ccebdc55bc09be81d137e7
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-06-21 15:50:53 -0400

    started adding code for the newKeep in the store

--- authenticate-me/frontend/src/store/keeps.js
+++ authenticate-me/frontend/src/store/keeps.js
@@ -1,5 +1,6 @@
 const CREATE_TASK = 'keeps/createTask';
 const UPDATE_TASK = 'keeps/updateTask';
+const CREATE_KEEP = 'keeps/createNewKeep';
 
 const createTask = (keep, taskId, task) => {
   return {
@@ -13,6 +14,15 @@ const createTask = (keep, taskId, task) => {
 const updateTask = (keep, taskId, task) => {
   return {
     type: UPDATE_TASK,
+    keep,
+    taskId,
+    task,
+  };
+};
+
+const createNewKeep = (keep, taskId, task) => {
+  return {
+    type: CREATE_KEEP,
     keep,
     taskId,
     task,
@@ -33,6 +43,16 @@ export const updateTaskThunk = (keep, taskId, task) => async (dispatch) => {
   //   if response is 200
   if (true) {
     dispatch(updateTask(keep, taskId, task));
+  }
+  //   return response;
+};
+
+export const createNewKeepThunk = (keep, task) => async (dispatch) => {
+  //   if response is 200
+  console.log(keep, task);
+  if (true) {
+    const randomTaskId = Math.random();
+    dispatch(createTask(keep, randomTaskId, task));
   }
   //   return response;
 };
@@ -103,6 +123,39 @@ const initialState = {
 const keepsReducer = (state = initialState, action) => {
   switch (action.type) {
     case CREATE_TASK:
+      return {
+        ...state,
+        [action.keep]: {
+          ...state[action.keep],
+          tasks: {
+            ...state[action.keep].tasks,
+            [action.taskId]: action.task,
+          },
+        },
+      };
+
+    case UPDATE_TASK:
+      return {
+        ...state,
+        [action.keep]: {
+          ...state[action.keep],
+          tasks: {
+            ...state[action.keep].tasks,
+            [action.taskId]: {
+              ...state[action.keep].tasks[action.taskId],
+              ...action.task,
+            },
+          },
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+const newKeepReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_KEEP:
       return {
         ...state,
         [action.keep]: {

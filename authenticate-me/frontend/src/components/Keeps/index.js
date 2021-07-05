238c795345dcd259f8da00285b5cb10fac37c9e4
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-07-05 16:01:36 -0400

    finished keepsStyling

--- authenticate-me/frontend/src/components/Keeps/index.js
+++ authenticate-me/frontend/src/components/Keeps/index.js
@@ -1,13 +1,8 @@
 import { useSelector } from 'react-redux';
-// import React, { useState } from 'react';
-// import Card from '../Ui/Card';
-//  Card & useState was not being used yet. React error
 import Keep from '../Keep';
 
 import './Keeps.css';
 
-// []TODO: pass in props to use in title, etc
-// []TODO: create a form to make a new keep item
 const Keeps = () => {
   const keeps = useSelector((state) => state.keeps);
 
@@ -15,7 +10,6 @@ const Keeps = () => {
     <div className="keepsList">
       {Object.keys(keeps).map((keepId) => (
         <Keep key={`keep-${keepId}`} id={keepId} />
-        // <Keep key={`keep-`} id="string" />
       ))}
     </div>
   );

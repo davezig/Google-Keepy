261b401c8daa6ff90d273bc78f9c6e0c0f747fb5
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-07-05 17:29:11 -0400

    finished models

---
+++ authenticate-me/backend/db/migrations/20210705212252-create-task.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      isComplete: {
+        type: Sequelize.BOOLEAN
+      },
+      position: {
+        type: Sequelize.INTEGER
+      },
+      keepId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tasks');
+  }
+};
